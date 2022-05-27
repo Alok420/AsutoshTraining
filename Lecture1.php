c419045dc7b64732b81e24c037207fe06e0cb454
kiranNagvadiya <32874450+Alok420@users.noreply.github.com>
2022-05-27 20:10:46 +0530

    First comm

---
+++ Lecture1.php
@@ -0,0 +1,14 @@
+<?php
+$name="Ashutosh verma";
+$enroll=109;
+echo "<strong>Your name is:</strong> ".$name." <br><strong> Enroll no is: </strong> ".$enroll;
+// -------------Arithmatic operator
+$num1=123;
+$num2="356";
+echo "<br>Arithmatic operation + :". ($num1+$num2);
+echo "<br>Arithmatic operation * :". ($num1*$num2);
+echo "<br>Arithmatic operation / :". ($num1/$num2);
+echo "<br>Arithmatic operation - :". ($num1-$num2);
+echo "<br>Arithmatic operation % :". ($num1%$num2);
+
+?>
